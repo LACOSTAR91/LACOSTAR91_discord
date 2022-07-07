d7d99e0662106d153747bcc4902ef5eabbf50c76
LACOSTAR91 <66567863+LACOSTAR91@users.noreply.github.com>
2022-07-07 23:44:21 +0200

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,22 @@
+const config = {
+    USERNAME: "LACOSTAR91#9999",
+    PICTURE: "assets/img/lacostar_PP.gif",
+
+    DESCRIPTION: "Hello everyone, I'm LACOSTAR91! 16 years old, french developper orient web, I lead, develop, moderate several communities and large servers. I speak French and English.",
+
+    BADGES: [
+        { id: "HypeSquad Bravery", image: "assets/img/badge_bravery.png" },
+        { id: "Nitro Sub", image: "assets/img/badge_nitro.png" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/logo_nightgroup.png", link: "https://discord.gg/jH6wB3B6s8", name: "Night Group - Owner" },
+        { id: "project-2", image: "assets/img/pi-heberg_logo.png", link: "https://discord.gg/ZSZwcyB6TA", name: "Pi-heberg - Director" },
+        { id: "project-5", image: "assets/img/logo_devcommunity.png", link: "https://discord.gg/RmFKX6khYK", name: "Development Community - Confirmed Support" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/LACOSTAR91" },
+        { class_name: "fab fa-opera", link: "https://night-api.com"}
+    ],
+}
